02196621fde1e71582c43ba529d2b658f8c07acf
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-08-19 15:53:33 +1000

    trivia page 2 started

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Intro1 from "./pages/QR1/Intro1";
 import Intro2 from "./pages/QR1/Intro2";
 import Trivia1 from "./pages/QR2/Trivia1";
+import Trivia2 from "./pages/QR2/Trivia2";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Intro1 />} />
           <Route path="/intro2" element={<Intro2 />} />
           <Route path="/trivia1" element={<Trivia1 />} />
+          <Route path="/trivia2" element={<Trivia2 />} />
           <Route
             path="*"
             element={
---
+++ src/pages/QR2/Trivia2.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import TriviaBackground from "../../assets/Backgrounds/Trivia-BG.png";
+import TextBox from "../../components/TextBox";
+
+const Trivia2: React.FC = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-center bg-cover font-mono"
+      style={{ backgroundImage: `url(${TriviaBackground})` }}
+    >
+      <Link to="/trivia1" className="text-customGray pt-2">
+        ← BACK
+      </Link>
+      <TextBox>
+        <p className="text-white">What does green-washing refer to?</p>
+        {/* <p className="text-white mt-4 mb-4">
+          THE ANSWERS LIE WITHIN THE FABRIC...{" "}
+        </p> */}
+      </TextBox>
+      <p className="text-customGray text-xl mb-2">Questions Here</p>
+      <p className="text-customGray uppercase mt-4 text-xl border border-customGray px-1">
+        <Link to="/trivia2" className="inline-block">
+          SUBMIT
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Trivia2;
