5dac164dae61f84e70bcd4e503fbde3bb82a221c
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-08-20 15:34:39 +1000

    more viewport testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import 'normalize.css/normalize.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Intro1 from "./pages/QR1/Intro1";
 import Intro2 from "./pages/QR1/Intro2";
