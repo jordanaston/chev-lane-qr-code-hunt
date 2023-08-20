3f2f867f31e5bb3aee85176ae593aba7f5faa351
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-08-20 15:46:59 +1000

    scroll anchoring added to trivia1 page

--- src/pages/QR2/Trivia1.tsx
+++ src/pages/QR2/Trivia1.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import TriviaBackground from "../../assets/Backgrounds/Trivia-BG.png";
 import ChevLaneLogo from "../../assets/Logos/Chev-Lane-Wavey-Logo.png";
 
 const Trivia1: React.FC = () => {
+  useEffect(() => {
+    // Calculate the middle of the page.
+    const middle =
+      (document.documentElement.scrollHeight - window.innerHeight) / 2;
+    window.scrollTo(0, middle);
+  }, []);
+
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen bg-center bg-cover font-mono"
